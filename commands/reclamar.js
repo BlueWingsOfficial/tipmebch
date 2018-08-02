39a8d28885b251634fd8a0a7a9a22fd8abff3007
BlueWingsOfficial <42040373+BlueWingsOfficial@users.noreply.github.com>
2018-08-02 17:55:13 -0400

    Update reclamar.js

--- commands/reclamar.js
+++ commands/reclamar.js
@@ -2,7 +2,7 @@ module.exports = async ({ ctx, reply, username }) => {
   await ctx.maybeReplyFromStickerSet('claim');
 
   await ctx.reply(
-    `¡Está Hecho! @${username} A partir de ahora  no necesita usar el comando \`/reclamar\` para recibir propinas`,
+    `¡Está Hecho! @${username} A partir de ahora  no necesitas usar el comando \`/reclamar\` para recibir propinas`,
     { parse_mode: 'markdown' }
   );
 };
