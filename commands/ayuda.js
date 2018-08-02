5720ae30c903413ab192985b9dbc3c24f10d384d
BlueWingsOfficial <42040373+BlueWingsOfficial@users.noreply.github.com>
2018-08-02 14:57:47 -0400

    Update ayuda.js

--- commands/ayuda.js
+++ commands/ayuda.js
@@ -12,7 +12,7 @@ module.exports = async ({ ctx, fetchRpc, userId, isPm, reply }) => {
       '/saldo - Muestra su saldo',
       '/deposito - Muestra tu dirección de depósito (PM)',
       '/retirar <billetera> [<Cantidad de BCH>|$<Cantidad de USD>|all> - Retirar fondos',
-      '/estadisticas - Mostrar las estadísticas de inflexión',
+      '/Estadisticas - Mostrar las estadísticas de inflexión',
       '/ayuda - Está en ayuda',
       '/informacion - Informacion sobre el bot',
       '/EstablecerStikers [name] - Establecer pack de stickerpara el canal (pepe or none)',
