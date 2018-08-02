09d658ab973f2a0d683759014cb5a4d1ed58447d
BlueWingsOfficial <42040373+BlueWingsOfficial@users.noreply.github.com>
2018-08-02 17:15:41 -0400

    Update ayuda.js

--- commands/ayuda.js
+++ commands/ayuda.js
@@ -7,13 +7,13 @@ module.exports = async ({ ctx, fetchRpc, userId, isPm, reply }) => {
 
   await reply(
     [
-      '/propina $1.23 - Enviar sugerencia',
-      '/propina 0.0001 - Enviar sugerencia (BCH)',
+      '/propina $1.23 - Enviar propina',
+      '/propina 0.0001 - Enviar propina (BCH)',
       '/saldo - Muestra su saldo',
       '/deposito - Muestra tu dirección de depósito (PM)',
-      '/retirar <billetera> [<Cantidad de BCH>|$<Cantidad de USD>|all> - Retirar fondos',
-      '/Estadisticas - Mostrar las estadísticas de inflexión',
-      '/ayuda - Está en ayuda',
+      '/retirar <cartera> [<Cantidad de BCH>|$<Cantidad de USD>|all> - Retirar fondos',
+      '/Estadisticas - Te muestras las estadísticas en relación a las propinas',
+      '/ayuda - Te muestra los comandos',
       '/informacion - Informacion sobre el bot',
       '/EstablecerStikers [name] - Establecer pack de sticker para el canal (pepe or none)',
     ].join('\n')
