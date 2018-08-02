7fb3b024326c0dc386b3d7d79e8a0d01a8171fa4
BlueWingsOfficial <42040373+BlueWingsOfficial@users.noreply.github.com>
2018-08-02 15:58:19 -0400

    Update propina.js

--- commands/propina.js
+++ commands/propina.js
@@ -20,7 +20,7 @@ module.exports = async ({
 }) => {
   if (params.length !== 2) {
     await reply(
-      `El comando que has usado no ha sido escrito correctamente. Sugiero (...) "/propina 0.01 @username" or "/propina $1 @username"`
+      `El comando que has usado no ha sido escrito correctamente. Sugiero (...) "/propina 0.01 @NombreDeUsuario" or "/propina $1 @NombreDeUsuario"`
     );
     await ctx.maybeReplyFromStickerSet('confused');
     return;
@@ -33,7 +33,7 @@ module.exports = async ({
   if (!toUserMatch) {
     console.warn(`Invalid username format for ${toUserRaw}`);
     await reply(
-      `Ese formato de nombre de usuario no es válido. Sugiero (...) /propina $1 @SomeUserName`
+      `Esel formato de nombre de usuario no es válido. Sugiero (...) /propina $1 @SomeUserName`
     );
     await ctx.maybeReplyFromStickerSet('confused');
     return;
@@ -48,7 +48,7 @@ module.exports = async ({
 
   if (!bchAmount) {
     await reply(
-      `No entiendo la cantidad. Sugiero (...) "/propina 0.01 @username" or "/propina $1 @username"`
+      `No No entiendo la cantidad. Sugiero (...) "/propina 0.01 @NombreDeUsuario" or "/propina $1 @NombreDeUsuario"`
     );
     await ctx.maybeReplyFromStickerSet('confused');
     return;
@@ -113,7 +113,7 @@ module.exports = async ({
 
   if (!userIsKnown) {
     await reply(
-      `@${toUsername} necesita reclamar la propina diciendo / reclamar. @${username} puede revertir la sugerencia con "/undo ${unclaimedId}" hasta ese momento`
+      `@${toUsername} necesita reclamar la propina diciendo / reclamar. @${NombreDeUsuario} puede revertir la sugerencia con "/undo ${unclaimedId}" hasta ese momento`
     );
   }
 
