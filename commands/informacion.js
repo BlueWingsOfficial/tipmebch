1bc1c83ee0b2242520836e9122f113a1cfb70a24
BlueWingsOfficial <42040373+BlueWingsOfficial@users.noreply.github.com>
2018-08-02 13:46:36 -0400

    Update and rename about.js to informacion.js

---
+++ commands/informacion.js
@@ -0,0 +1,16 @@
+const { version } = require('../package.json');
+
+module.exports = async ({ ctx, fetchRpc, userId, isPm, reply }) => {
+  await ctx.maybeReplyFromStickerSet('about');
+
+  await reply(
+    [
+      `Soy un bot (v${version}) escrito por Andreas Brekken (@abrkn) para inclinar Bitcoin Cash (BCH) en Telegram`,
+      'Pruebe el comando /ayuda en un mensaje privado',
+      `Puedes darme propina y se irá al grifo una vez esté listo.`,
+      `Soy un código abierto: https://github.com/abrkn/tipmebch`,
+      `Reportar errores aquí: https://github.com/abrkn/tipmebch/issues`,
+      `El bot no es una billetera. Sus fondos se perderán si hay errores`,
+    ].join('\n')
+  );
+};
