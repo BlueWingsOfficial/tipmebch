c998fa19f6bf5b3cc9f84eee2e79ac5dad56c758
BlueWingsOfficial <42040373+BlueWingsOfficial@users.noreply.github.com>
2018-08-02 17:51:25 -0400

    Update informacion.js

--- commands/informacion.js
+++ commands/informacion.js
@@ -10,7 +10,7 @@ module.exports = async ({ ctx, fetchRpc, userId, isPm, reply }) => {
       `Si me envías una propina, la misma irá a parar a los fondos del grifo de Bitcoin Cash (cuando esté listo)`,
       `Soy una aplicación de código abierto: https://github.com/abrkn/tipmebch`,
       `Reporta errores aquí: https://github.com/abrkn/tipmebch/issues`,
-      `El bot no esta diseñado para ser utilizado como cartera. Usted podría perder el saldo que tiene depositado en él a causa de un error`,
+      `El bot no esta diseñado para ser utilizado como cartera. Usted podría perder el saldo que tiene depositado en él a causa de algún error`,
     ].join('\n')
   );
 };
